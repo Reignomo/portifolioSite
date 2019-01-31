81e760956a9e8565d5a06f2538adb5a463c1fea8
Evandro <eignacio403@gmail.com>
2019-01-30 23:19:57 -0300

    ..

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
     <head>
         <link type="text/css" rel="stylesheet" href="CSS/style.css"  media="screen,projection"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
         <title>Evandro Ignácio</title>
     </head>
     <body>
