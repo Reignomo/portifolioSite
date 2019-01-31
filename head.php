67fac0436342d60e322d16b88d5f67a802270cc4
Evandro <eignacio403@gmail.com>
2019-01-30 22:27:50 -0300

    mundando o layout

--- head.php
+++ head.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
     <head>
-        <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-            <title>Evandro Ignácio</title>
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <!--JavaScript at end of body for optimized loading-->
-        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="CSS/style.css"  media="screen,projection"/>
+        <title>Evandro Ignácio</title>
     </head>
     <body>
--- index.php
+++ index.php
@@ -1,35 +1,86 @@
 <?php include_once("head.php")?>
 <?php include_once("cabeçalho.php")?>
-<style>
- body{
-     background-image:url(imgs/programacao01.jpg);
-     background-repeat: no-repeat;
-     background-attachment: fixed;
-     background-position: bottom;
- }
-
- #banner{
-    text-align: center;
-    padding-top:260px;
-    text-transform: uppercase;
-    color:white;
-    text-shadow: black 0.2em 0.4em 0.3em;
-    height:1200;
- }
- #sobre{
-
- }
-</style>
-<div style="min-height:865px;">
-	<div class="container">
-        <section id="banner">
-            <h4>Desenvolvedor Web</u></h4>
-            <hr>
-            <p>Back - end</p>
+    <header class="main-header">
+        <a href="index.php" class="main-header-lik">
+            <img src="img/evandro.jpg" alt="Evandro Ignácio">
+        </a>
+        <h1>
+            Portifólio<br>
+            Desenvolvedor Web.<br>
+            Back - End.<br>
+            Por<strong> Evandro Ignácio</strong>
+        </h1>
+    </header>
+    <main class="main-contet">
+        <section class="main-content-section">
+            <h2>Sobre</h2>
+            <p>Se você precisa de ajuda em alguma das áreas mencionadas abaixo, conte-me sua necessidade. Terei prazer em ajudá-lo(a) a planejar e implementar uma estratégia digital eficaz para o seu negócio.</p>
+            <p>Saiba que ter um site hoje não é mais um diferencial de uma marca, mas, sim, uma obrigação. Digamos que o site é um cartão de visitas, que fortalece ou começa a estabelecer a presença no mundo digital de uma empresa.</p>
+            <a href="#" title="veja mais" class="button-default"> Ver mais</a>
         </section>
-        <section id="sobre">
-        <center><h3><u>Sobre</u><h3></center>
+        <section class="main-content-section">
+            <h3>Trabalhos</h3>
+            <article class="recent-works-card">
+                <a href="img/works.jpg"><img src="img/works.jpg" alt="Trabalhos"></a>
+                <p class="recent-works-card-title">Titulo do trabalho</p>
+                <p class="recent-works-card-content"> descrição do trabalho</p>
+            </article>
+            
+            <article class="recent-works-card">
+                <a href="img/works.jpg"><img src="img/works.jpg" alt="Trabalhos"></a>
+                <p class="recent-works-card-title">Titulo do trabalho 2</p>
+                <p class="recent-works-card-content"> descrição do trabalho</p>
+            </article>
+            
+            <article class="recent-works-card">
+                <a href="img/works.jpg"><img src="img/works.jpg" alt="Trabalhos"></a>
+                <p class="recent-works-card-title">Titulo do trabalho 3</p>
+                <p class="recent-works-card-content"> descrição do trabalho</p>
+            </article>
+            
+            <article class="recent-works-card">
+                <a href="img/works.jpg"><img src="img/works.jpg" alt="Trabalhos"></a>
+                <p class="recent-works-card-title">Titulo do trabalho 4</p>
+                <p class="recent-works-card-content"> descrição do trabalho</p>
+            </article>
+            
+            <a href="#" title="veja mais" class="button-default">Veja o portifolio completo</a>
+            
         </section>
-    </div>
-</div>
-<?php include_once("rodape.php")?>
+        
+        <!-- todas a outras sections ficaram aqui -->
+            
+        <section class="main-content-section">
+            <h3>Entre em contato</h3>
+            <form action ="#" method="POST">
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" id="nome">
+                
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email">
+                
+                <label for="nome">Nome</label>
+                <textarea name="mensagem" id="mensagem"></textarea>
+                    
+                <input type="submit" value="Enviar">
+            </form>
+            <address>
+                <p>
+                    <span>Endereço</span>
+                    Encruzilhada - Santos, SP
+                </p>
+                <p>
+                    <span>Telefone</span>
+                    (13)997125069
+                </p>
+                <p>
+                    <span>E-mail</span>
+                    eignacio403@gmail.com
+                </p>
+            </address>
+        </section>
+    </main>
+    <footer class="main-footer">
+    </footer>
+
+    <?php include_once("rodape.php")?>
