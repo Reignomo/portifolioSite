5b6985cf4e1fe99a68cf9fff98b367b71afb51e0
Evandro <eignacio403@gmail.com>
2019-02-26 21:28:35 -0300

    ..

--- public/rodape.php
+++ public/rodape.php
@@ -1,13 +1,15 @@
-        <footer>    
-            <div class="row">
-                <ul class="ul-icons">
-                    <li><a href="#" class="icon-link"><i class="fab fa-facebook"></i></a></li>
-                    <li><a href="#" class="icon-link"><i class="fab fa-github"></i></a></li>
-                    <li><a href="#" class="icon-link"><i class="fab fa-linkedin"></i></a></li>
-                </ul>    
-            </div>
-            <div class="row">
-                <b><p>Todos os direitos reservados ©</p> - <p class="footer-author">Evandro Ignácio</p></b>
+        <footer>
+            <div class="footer-contet">    
+                <div class="row">
+                    <ul class="ul-icons">
+                        <li><a href="#" class="icon-link"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="#" class="icon-link"><i class="fab fa-github"></i></a></li>
+                        <li><a href="#" class="icon-link"><i class="fab fa-linkedin"></i></a></li>
+                    </ul>    
+                </div>
+                <div class="row">
+                    <b><p>Todos os direitos reservados ©</p> - <p class="footer-author">Evandro Ignácio</p></b>
+                </div>
             </div>
         </footer>
     </body>
